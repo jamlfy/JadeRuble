72fd29650e62149adcbfcef5de602ddacc15596d
AlejoNext <fotocopia@galeph.com>
2012-08-04 21:58:53 -0500

    Sopport Jade NPM

--- commands/check_syntax.rb
+++ commands/check_syntax.rb
@@ -9,7 +9,7 @@ command t(:check_syntax) do |cmd|
 	# TODO Check syntax against file contents, so we pick up unsaved changes!
 		context.exit_show_tool_tip('File not found') unless File.exists?(ENV['TM_FILEPATH'])
 	# TODO mirar si el check funciona!
-		result = IO.popen("jade --path --pretty '#{ENV['TM_FILEPATH']}' 2>&1") {|io| io.read }
+		result = IO.popen("jade '#{ENV['TM_FILEPATH']}' --out /tmp ") {|io| io.read }
 		if result.nil? or result.strip.length == 0
 			"No output from 'Jade'. Please be sure the Jade are installed. Install 'npm -g install jade'"
 		else
