d64ca9bc783a998697deeb2c9fcc9b9b47e1a9ed
AlejoNext <fotocopia@galeph.com>
2012-10-26 20:58:27 -0500

    Mejorando la cosa

--- templates/templates.rb
+++ templates/templates.rb
@@ -1,10 +1,25 @@
 require 'ruble'
 
-template t(:jade_template) do |t|
+template t(:template_new) do |t|
 	t.filetype = "*.jade"
 	t.invoke do |context|
 		ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
-		raw_contents = IO.read("#{File.dirname(ENV['TM_BUNDLE_SUPPORT'])}/templates/template.jade")
+		raw_contents = IO.read("#{File.dirname(ENV['TM_BUNDLE_SUPPORT'])}/templates/new.jade")
 		raw_contents.gsub(/\$\{([Creating a new template^}]*)\}/) {|match| ENV[match[2..-2]] }
-  end
+	end
+end
+template t(:template_extend) do |t|
+	t.filetype = "*.jade"
+	t.invoke do |context|
+		raw_contents = IO.read("#{ENV['TM_BUNDLE_PATH']}/templates/extend.jade")
+		raw_contents.gsub(/${([^}]*)}/) {|match| ENV[match[2..-2]] }
+	end
+end
+
+template t(:template_mix) do |t|
+	t.filetype = "*.jade"
+	t.invoke do |context|
+		raw_contents = IO.read("#{ENV['TM_BUNDLE_PATH']}/templates/mix.jade")
+		raw_contents.gsub(/${([^}]*)}/) {|match| ENV[match[2..-2]] }
+	end
 end
